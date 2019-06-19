20b30e62f521c599544a6faba7bc860c2596bfb6
Paige Niedringhaus <paigen11@gmail.com>
2019-06-19 10:08:33 -0400

    building burgers

---
+++ src/containers/BurgerBuilder/BurgerBuiilder.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuiilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
+    },
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Burger ingredients={this.state.ingredients} />
+        <div>Build Controls</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuiilder;
