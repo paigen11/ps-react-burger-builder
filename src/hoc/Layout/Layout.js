a715b087dea8ba0c0d9dbecee96d7d30d866d131
Paige Niedringhaus <paigen11@gmail.com>
2019-07-20 18:25:04 -0400

    renaming and moving stuff around

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Aux from '../Aux/Aux';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+import classes from './Layout.css';
+
+class layout extends Component {
+  state = {
+    showSideDrawer: false,
+  };
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  sideDrawerOpenHandler = () => {
+    this.setState(prevState => {
+      return { showSideDrawer: !this.state.showSideDrawer };
+    });
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerOpenHandler} />
+        <SideDrawer
+          closed={this.sideDrawerClosedHandler}
+          open={this.state.showSideDrawer}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+
+export default layout;
