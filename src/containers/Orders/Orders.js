efa9c07f406d7cb800fe2b5cfe526a59181e54e8
Paige Niedringhaus <paigen11@gmail.com>
2019-10-24 11:39:50 -0400

    adding order functionality

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import axios from '../../axios-orders';
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true,
+  };
+  componentDidMount() {
+    axios
+      .get('/orders.json')
+      .then(response => {
+        const fetchedOrders = [];
+        for (let key in response.data) {
+          fetchedOrders.push({ ...response.data[key], id: key });
+        }
+        this.setState({ loading: false, orders: fetchedOrders });
+      })
+      .catch(err => {
+        this.setState({ loading: false });
+      });
+  }
+
   render() {
     return (
       <div>
@@ -12,4 +33,4 @@ class Orders extends Component {
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
