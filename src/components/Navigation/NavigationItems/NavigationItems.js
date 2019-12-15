820c74259f8421e8cc839748fb34b317092f58a7
Paige Niedringhaus <paigen11@gmail.com>
2019-12-14 23:37:27 -0500

    wip commit

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,9 @@ import classes from './NavigationItems.css';
 const navigationItems = props => (
   <ul className={classes.NavigationItems}>
     <NavigationItem link="/">Burger Builder</NavigationItem>
-    <NavigationItem link="/orders">Orders</NavigationItem>
+    {props.isAuthenticated ? (
+      <NavigationItem link="/orders">Orders</NavigationItem>
+    ) : null}
     {!props.isAuthenticated ? (
       <NavigationItem link="/auth">Authenticate</NavigationItem>
     ) : (
