66d6a012bb2681034082a1027e95845158bb3929
Paige Niedringhaus <paigen11@gmail.com>
2019-12-21 16:37:01 -0500

    clean up

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -56,7 +56,7 @@ export const auth = (email, password, isSignup) => {
     axios
       .post(url, authData)
       .then(res => {
-        console.log(res);
+        // console.log(res);
         const expirationDate = new Date(
           new Date().getTime() + res.data.expiresIn * 1000
         );
@@ -67,7 +67,7 @@ export const auth = (email, password, isSignup) => {
         dispatch(checkAuthTimeout(res.data.expiresIn));
       })
       .catch(err => {
-        console.log(err);
+        // console.log(err);
         dispatch(authFail(err.response.data.error));
       });
   };
