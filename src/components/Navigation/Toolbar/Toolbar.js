83da03706e79ffe48b83873dc3e27d25fe3fdd14
Paige Niedringhaus <paigen11@gmail.com>
2019-12-29 10:45:29 -0500

    adding new nav updates

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,7 +6,10 @@ import classes from './Toolbar.css';
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <DrawerToggle clicked={props.drawerToggleClicked} />
+    <DrawerToggle
+      clicked={props.drawerToggleClicked}
+      isAuthenticated={props.isAuth}
+    />
     <div className={classes.Logo}>
       <Logo />
     </div>
