cafbeb4cc3d7efbbcaf43db366c7988562846fe0
Paige Niedringhaus <paigen11@gmail.com>
2020-02-26 17:27:25 -0500

    adding refactored order form

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import Order from '../../components/Order/Order';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
@@ -7,25 +7,24 @@ import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
 
 const Orders = props => {
-  componentDidMount() {
-    this.props.onFetchOrders(this.props.token, this.props.userId);
+  useEffect(() => {
+    props.onFetchOrders(props.token, props.userId);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  let orders = <Spinner />;
+  if (!props.loading) {
+    orders = props.orders.map(order => (
+      <Order
+        key={order.id}
+        ingredients={order.ingredients}
+        price={order.price}
+      />
+    ));
   }
 
-  render() {
-    let orders = <Spinner />;
-    if (!this.props.loading) {
-      orders = this.props.orders.map(order => (
-        <Order
-          key={order.id}
-          ingredients={order.ingredients}
-          price={order.price}
-        />
-      ));
-    }
-
-    return <div>{orders}</div>;
-  }
-}
+  return <div>{orders}</div>;
+};
 
 const mapStateToProps = state => {
   return {
