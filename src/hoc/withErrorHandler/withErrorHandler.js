2593b27cfab8a83d68e5e4aa452793a2bdc7dc41
Paige Niedringhaus <paigen11@gmail.com>
2020-03-08 11:20:59 -0400

    modifying hoc

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,37 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Aux/Aux';
+import useHttpErrorHandlerHook from '../../hooks/httpErrorHandlerHook';
 
 const withErrorHandler = (WrappedComponent, axios) => {
   return props => {
-    const [error, setError] = useState(null);
-
-    const reqInterceptor = axios.interceptors.request.use(req => {
-      setError(null);
-      return req;
-    });
-
-    const responseInterceptor = axios.interceptors.response.use(
-      res => res,
-      err => {
-        setError(err);
-      }
-    );
-
-    useEffect(() => {
-      return () => {
-        axios.interceptors.request.eject(reqInterceptor);
-        axios.interceptors.response.eject(responseInterceptor);
-      };
-    }, [reqInterceptor, responseInterceptor]);
-
-    const errorConfirmedHandler = () => {
-      setError(null);
-    };
-
+    const [error, clearError] = useHttpErrorHandlerHook();
     return (
       <Aux>
-        <Modal show={error} modalClosed={errorConfirmedHandler}>
+        <Modal show={error} modalClosed={clearError}>
           {error ? error.message : null}
         </Modal>
         <WrappedComponent {...props} />;
